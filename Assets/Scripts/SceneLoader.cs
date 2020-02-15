df339960ef30ebbaab7a021964ffd6a15abf237f
Dan <danlechambre@icloud.com>
2020-02-15 15:17:50 +0000

    Create SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private AudioSource music;
+    private Scene currentScene;
+
+    void Start()
+    {
+        music = FindObjectOfType<AudioSource>();
+        currentScene = SceneManager.GetActiveScene();
+        
+        if (currentScene.buildIndex == 0)
+        {
+            StartCoroutine(SplashToStartTransition());
+        }
+    }
+
+    IEnumerator SplashToStartTransition()
+    {
+        yield return new WaitForSeconds(music.clip.length + 0.5f);
+        SceneManager.LoadScene(1);
+    }
+}
